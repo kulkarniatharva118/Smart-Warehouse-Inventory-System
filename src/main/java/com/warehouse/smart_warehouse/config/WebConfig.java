68d44d06e827b96fbb655d2026e4b09ce8519629
Atharva Kulkarni <kulkarniatharva118@gmail.com>
2026-09-14 17:59:14 +0530

    CORS

--- src/main/java/com/warehouse/smart_warehouse/config/WebConfig.java
+++ src/main/java/com/warehouse/smart_warehouse/config/WebConfig.java
@@ -21,7 +21,8 @@ public class WebConfig {
                                 "http://localhost:5500",
                                 "http://127.0.0.1:5500",
                                 "http://localhost:5173",
-                                "http://127.0.0.1:5173"
+                                "http://127.0.0.1:5173",
+                                "smart-warehouse-inventory-system.vercel.app"
                         )
                         .allowedMethods(
                                 "GET",
